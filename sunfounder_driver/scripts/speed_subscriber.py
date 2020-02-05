95058efe8d2346d06600170b0d41828954312275
Nich <nicholas.vercruysse@gmail.com>
2020-02-04 20:07:16 -0700

    trying to get picar to work

--- sunfounder_driver/scripts/speed_subscriber.py
+++ sunfounder_driver/scripts/speed_subscriber.py
@@ -2,16 +2,18 @@
 import rospy
 from std_msgs.msg import Byte
 from SunFounder_PiCar.picar import back_wheels
+from SunFounder_PiCar import picar
 
 class BackWheels:
 
     def __init__(self):
         self.bwheels = back_wheels.Back_Wheels(debug=False)
-        self.bwheels.forward()
+        picar.setup()
 
     def callback(self, data):
         rospy.loginfo('received speed: {}'.format(data.data))
         self.bwheels.speed = data.data
+        self.bwheels.forward()
 
     def listener(self):
         rospy.init_node('speed_listener', anonymous=True)
