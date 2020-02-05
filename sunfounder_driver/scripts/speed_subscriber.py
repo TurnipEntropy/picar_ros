f4987dee78e684038ed1e63dd87f1299f9de0beb
Nich <nicholas.vercruysse@gmail.com>
2020-02-04 19:35:15 -0700

    subscriber controls wheels now

--- sunfounder_driver/scripts/speed_subscriber.py
+++ sunfounder_driver/scripts/speed_subscriber.py
@@ -3,15 +3,22 @@ import rospy
 from std_msgs.msg import Byte
 from SunFounder_PiCar.picar import back_wheels
 
-def callback(data):
-    rospy.loginfo('received speed: {}'.format(data.data))
-    bwheels = back_wheels.Back_Wheels()
+class BackWheels:
 
-def listener():
-    rospy.init_node('speed_listener', anonymous=True)
-    rospy.Subscriber('pwm', Byte, callback)
-    rospy.spin()
+    def __init__(self):
+        self.bwheels = back_wheels.Back_Wheels(debug=False)
+        self.bwheels.forward()
+
+    def callback(self, data):
+        rospy.loginfo('received speed: {}'.format(data.data))
+        self.bwheels.speed(data.data)
+
+    def listener(self):
+        rospy.init_node('speed_listener', anonymous=True)
+        rospy.Subscriber('pwm', Byte, self.callback)
+        rospy.spin()
 
 
 if __name__ == '__main__':
-    listener()
+    bw = BackWheels()
+    bw.listener()
